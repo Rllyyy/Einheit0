df01ece65f9c107649161c3e181da7bf0ffa0119
NiklasLot <nikii17.06@gmail.com>
2020-04-22 20:01:57 +0200

    Updated test.py to include functions, modules and classes

--- Test.py
+++ Test.py
@@ -1,5 +1,5 @@
 #This file is just for testing
-#Last edited: 20.04.2020
+#Last edited: 22.04.2020
 #Functioning programms are inbetween hashtags
 #Functions can be activated by removing the """ under the hashtags and before the next hashtags
 
@@ -43,14 +43,38 @@ print(s)
 """
 
 ################### Dictonaries
+"""
 ages = {"Alice": 20, "Bob":27}
 ages["Charlie"]=30
 ages["Alice"] +=1
 print(ages)
+"""
 
+#################### Functions
+"""
+def square (x):
+    return x * x
+def main():
+        for i in range(10):
+            #First Bracket is placeholder for i, last playeholder is for Square. Following Format used in Python versions before 3.6
+            print ("{} square is {} (old)".format (i, square (i)))
+            #Following format can be used in Versions after 3.6. First Version will work too.
+            print (f"{i} square is {square(i)}")
+if __name__ == "__main__":
+    main()
+"""
 
+################## Modules
+#From filename import function
 
-
+################## Classes
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+p = Point (3,5)
+print(p.x)
+print(p.y)
 
 
 
